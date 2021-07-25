5ac2461cd0c3614be377d0345fa33b83cf032ef6
Pwako11 <akolo@hotmail.com>
2021-07-25 05:14:55 -0400

    Added ActionController inheriting Cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::API
 
 include ::ActionController::Cookies 
-    # include ActionController::Helpers
-    # helpers do 
         def current_user
             User.find_by(id: session[:user_id])
-            
-            # if session[:user_id]
-            #     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-            # end 
         end 
 
         def logged_in?
             !!current_user
         end 
-    # end
 end
