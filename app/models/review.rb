2abbc21e480dfb2b1ed8e07aa9249def9283e9e2
Pwako11 <akolo@hotmail.com>
2021-09-01 16:42:46 -0400

    Updated reviews model to auto delete dependents

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,8 @@ class Review < ApplicationRecord
     belongs_to :movie, required: false
 
     has_many :movies, through: :recommendations, class_name: "Movie"
-    has_many :users, through: :recommendations
+    has_many :users, through: :recommendations 
+    has_many :recommendations, dependent: :destroy
 
     validates_presence_of :content, :rate
 end
